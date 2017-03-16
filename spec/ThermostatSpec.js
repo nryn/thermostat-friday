b2995775c891d0846b67d7d725eec651fd495640
Noora Al Qasimi <n.k.alqasimi@gmail.com>
2017-03-16 11:01:04 +0000

    User Story 7 - 11
    As a user
    So I can choose between different power modes
    I would like to be able to switch back to power saving mode on
    
    As a user
    So I can choose between different power modes
    I would like to see a maximum temperature when power saving mode is on
    
    As a user
    So I can choose between different power modes
    I would like to see a maximum temperature when power saving mode is off
    
    As a user
    So I can set the temperature to default
    I would like to be able to reset the temperature
    
    As a user
    So I can monitor the energy usage
    I would like to be able to see the level of energy usage

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -10,8 +10,8 @@ describe('Thermostat', function() {
   // As a user
   // So I can see the starting temperature
   // I would like the thermostat to start at 20 degrees
-  it('has a starting temperature of 20 degrees', function() {
-    expect(thermostat.getCurrentTemp()).toEqual(20);
+  it('has a starting temperature', function() {
+    expect(thermostat.getCurrentTemp()).toEqual(thermostat.DEFAULT_TEMP);
   });
 
   // As a user
@@ -19,7 +19,7 @@ describe('Thermostat', function() {
   // I would like to have an increase temperature function on thermostat
   it('can increase temperature', function() {
     thermostat.increaseTemp();
-    expect(thermostat.getCurrentTemp()).toEqual(21);
+    expect(thermostat.getCurrentTemp()).toEqual(thermostat.DEFAULT_TEMP + 1);
   });
 
   // As a user
@@ -27,17 +27,17 @@ describe('Thermostat', function() {
   // I would like to have a decrease temperature function on thermostat
   it('can decrease temperature', function() {
     thermostat.decreaseTemp();
-    expect(thermostat.getCurrentTemp()).toEqual(19);
+    expect(thermostat.getCurrentTemp()).toEqual(thermostat.DEFAULT_TEMP - 1);
   });
 
   // As a user
   // So I can know the lowest temperature setting
   // I want to be able to see a minimum temperature
-  it('has a minimum temperature of 10 degrees', function() {
+  it('has a minimum temperature', function() {
   for (var i = 0; i < 10; i++) {
     thermostat.decreaseTemp();
   }
-  expect(thermostat.getCurrentTemp()).toEqual(10);
+  expect(thermostat.getCurrentTemp()).toEqual(thermostat.MINIMUM_TEMPERATURE);
   });
 
 
@@ -65,4 +65,52 @@ describe('Thermostat', function() {
     thermostat.switchPowerSavingModeOn();
     expect(thermostat.isPowerSavingModeOn()).toBe(true);
   });
-}); //  Main describe
+
+  // As a user
+  // So I can choose between different power modes
+  // I would like to see a maximum temperature when power saving mode is on
+  describe('when power saving mode on', function() {
+      it('has a maximum temperature ', function() {
+        for (var i = 0; i < 10; i++) {
+          thermostat.increaseTemp();
+        }
+        expect(thermostat.getCurrentTemp()).toEqual(thermostat.MAX_TEMP_PSM_ON);
+      });
+  });
+
+  // As a user
+  // So I can choose between different power modes
+  // I would like to see a maximum temperature when power saving mode is off
+  describe('when power saving mode off', function() {
+      it('has a maximum temperature', function() {
+        thermostat.switchPowerSavingModeOff();
+        for (var i = 0; i < 20; i++) {
+          thermostat.increaseTemp();
+        }
+        expect(thermostat.getCurrentTemp()).toEqual(thermostat.MAX_TEMP_PSM_OFF);
+      });
+  });
+
+  // As a user
+  // So I can set the temperature to default
+  // I would like to be able to reset the temperature
+  it('can reset the temperature', function() {
+    for (var i = 0; i < 10; i++) {
+      thermostat.increaseTemp();
+    }
+    thermostat.reset();
+    expect(thermostat.getCurrentTemp()).toEqual(thermostat.DEFAULT_TEMP);
+  });
+
+  // As a user
+  // So I can monitor the energy usage
+  // I would like to be able to see the level of energy usage
+  it('shows current energy usage', function() {
+    thermostat.currentTemp = thermostat.LOW_USAGE_THRESHOLD - 1
+    expect(thermostat.getEnergyUsage()).toEqual('low-usage');
+    thermostat.currentTemp = thermostat.MEDIUM_USAGE_THRESHOLD - 1
+    expect(thermostat.getEnergyUsage()).toEqual('medium-usage');
+    thermostat.currentTemp = thermostat.MEDIUM_USAGE_THRESHOLD + 1
+    expect(thermostat.getEnergyUsage()).toEqual('high-usage');
+  });
+});
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,11 +1,18 @@
+debugger;
 'use strict';
 
 function Thermostat() {
-  this.START_TEMP = 20;
+  this.DEFAULT_TEMP = 20;
   this.DEFAULT_CHANGE = 1;
   this.MINIMUM_TEMPERATURE = 10;
-  this.currentTemp = this.START_TEMP;
+  this.MAX_TEMP_PSM_OFF = 32;
+  this.MAX_TEMP_PSM_ON = 25;
+  this.LOW_USAGE_THRESHOLD = 18;
+  this.MEDIUM_USAGE_THRESHOLD = 25;
+
+  this.currentTemp = this.DEFAULT_TEMP;
   this.powerSavingMode = true;
+
 };
 
   Thermostat.prototype.getCurrentTemp = function() {
@@ -13,6 +20,9 @@ function Thermostat() {
   };
 
   Thermostat.prototype.increaseTemp = function() {
+    if (this.isMaximumTemperature()) {
+      return;
+    }
     this.currentTemp += this.DEFAULT_CHANGE;
   };
 
@@ -37,4 +47,27 @@ function Thermostat() {
 
   Thermostat.prototype.switchPowerSavingModeOn = function() {
     this.powerSavingMode = true;
+  };
+
+  Thermostat.prototype.isMaximumTemperature = function() {
+    if (this.isPowerSavingModeOn() === false) {
+      return this.currentTemp === this.MAX_TEMP_PSM_OFF;
+  }
+    return this.currentTemp === this.MAX_TEMP_PSM_ON;
+  };
+
+  Thermostat.prototype.reset = function() {
+    this.currentTemp = this.DEFAULT_TEMP
+  };
+
+  Thermostat.prototype.getEnergyUsage = function() {
+    if (this.currentTemp < this.LOW_USAGE_THRESHOLD) {
+      return 'low-usage';
+    }
+    else if (this.currentTemp < this.MEDIUM_USAGE_THRESHOLD) {
+      return 'medium-usage';
+    }
+    else {
+      return 'high-usage';
+    }
   };
